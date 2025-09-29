c3d1cde01885568f43da83f3ff7b36103f3f47a4
chivasxian <chivasxian@gmail.com>
2025-09-29 15:45:11 +0800

    2nd commit

---
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Resources\Posts\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class PostForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //option 1
+                // TextInput::make('user_id')
+                //     ->hidden() //not recommended! it will be viewable in the source code
+                //     ->default(auth()->id())
+                //     ->required()
+                //     ->numeric(),
+
+                
+                TextInput::make('title')
+                    ->required(),
+                Textarea::make('content')
+                    ->required()
+                    ->columnSpanFull(),
+                Toggle::make('is_published')
+                    ->required(),
+            ]);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+use Filament\Auth\MultiFactor\App\Contracts\HasAppAuthentication;
+use Filament\Auth\MultiFactor\App\Contracts\HasAppAuthenticationRecovery;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser, HasAppAuthentication, HasAppAuthenticationRecovery, MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -31,6 +36,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'app_authentication_secret',
+        'app_authentication_recovery_codes',
     ];
 
     /**
@@ -43,6 +50,57 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'app_authentication_secret' => 'encrypted',
+            'app_authentication_recovery_codes' => 'encrypted:array',
         ];
+    }
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
+    public function getAppAuthenticationSecret(): ?string
+    {
+        // This method should return the user's saved app authentication secret.
+    
+        return $this->app_authentication_secret;
+    }
+
+    public function saveAppAuthenticationSecret(?string $secret): void
+    {
+        // This method should save the user's app authentication secret.
+    
+        $this->app_authentication_secret = $secret;
+        $this->save();
+    }
+
+    public function getAppAuthenticationHolderName(): string
+    {
+        // In a user's authentication app, each account can be represented by a "holder name".
+        // If the user has multiple accounts in your app, it might be a good idea to use
+        // their email address as then they are still uniquely identifiable.
+    
+        return $this->email;
+    }
+    public function getAppAuthenticationRecoveryCodes(): ?array
+    {
+        // This method should return the user's saved app authentication recovery codes.
+    
+        return $this->app_authentication_recovery_codes;
+    }
+    
+    /*
+    @param  array<string> | null  $codes
+     */
+    public function saveAppAuthenticationRecoveryCodes(?array $codes): void
+    {
+        // This method should save the user's app authentication recovery codes.
+    
+        $this->app_authentication_recovery_codes = $codes;
+        $this->save();
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
